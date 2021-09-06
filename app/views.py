2d2437a14d524f19709e2838e3e53ae727f7cf04
PawanSadhwani17 <84863380+PawanSadhwani17@users.noreply.github.com>
2021-09-06 21:34:44 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -9,6 +9,9 @@ import random
 import os
 
 
+# Adding path to config
+app.config['INITIAL_FILE_UPLOADS'] = 'app/static/uploads'
+
 # Loading model
 model = models.load_model('app/static/model/intel_image.h5')
 
